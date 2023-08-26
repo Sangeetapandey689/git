7671ca94e399112486c1e821f7134b1bea30636e
Sangeetapandey689 <140141092+Sangeetapandey689@users.noreply.github.com>
2023-08-26 19:08:02 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('sangeeta');
+    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Massage</title><head>');
+        res.write('<body><form action="/massage" method="POST"><input type="text"><button type="submit">send</form><body>');
+        res.write('</html>');
+        res.end();  
+    }
+    // process.exit();
+    if(url === '/massage' && method === 'POST') {
+        const body = [];
+       req.on('data', (chunk) =>{
+        console.log(chunk);
+        body.push(chunk);
+
+      });
+      return req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFile('massage.text', message, (error) =>{
+            res.statusCode = 302;
+            res.setRedirct('Location', '/');
+            return res.end();
+        });
+      });
+    }
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title><head>');
+    res.write('<body><h1>Hello from my node js server</h1><body>');
+    res.write('</html>');
+    res.end();
+});
+server.listen(3000,() =>{
+console.log('server is running');
 });
 
-server.listen(4000, () => {
-    console.log('Server is listening on port 4000');
-});
