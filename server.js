1263867a4d14c50df57217aeef264ed5d2f31be3
Sangeetapandey689 <140141092+Sangeetapandey689@users.noreply.github.com>
2023-08-27 12:53:36 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,11 @@
 const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    const url = req.url;
-    const method = req.method;
-    if(url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter Massage</title><head>');
-        res.write('<body><form action="/massage" method="POST"><input type="text"><button type="submit">send</form><body>');
-        res.write('</html>');
-        res.end();  
-    }
-    // process.exit();
-    if(url === '/massage' && method === 'POST') {
-        const body = [];
-       req.on('data', (chunk) =>{
-        console.log(chunk);
-        body.push(chunk);
+const route = require('./route');
 
-      });
-      return req.on('end', () => {
-        const parsedBody = Buffer.concat(body).toString();
-        const message = parsedBody.split('=')[1];
-        fs.writeFile('massage.text', message, (error) =>{
-            res.statusCode = 302;
-            res.setRedirct('Location', '/');
-            return res.end();
-        });
-      });
-    }
-    res.setHeader('content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title><head>');
-    res.write('<body><h1>Hello from my node js server</h1><body>');
-    res.write('</html>');
-    res.end();
-});
-server.listen(3000,() =>{
-console.log('server is running');
+console.log(route.someText);
+
+const server = http.createServer(route.handler);
+
+server.listen(3000,() => {
+  console.log('server is running 4000');
 });
 
